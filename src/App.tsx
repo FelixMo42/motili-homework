32f499fa790c0191b52649ef0d868a8ca0b27258
FelixMo42 <felix@tacocat.com>
2021-08-04 13:18:49 -0700

    got basic github search features working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import styles from './App.module.css'
+
+interface GitHubRepo {
+    id: number,
+    name: string
+}
+
+interface GitHubSearchResponse {
+    items: GitHubRepo[]
+}
 
 export default function App() {
+    let [searchResults, setSearchResults] = useState<GitHubRepo[]>([])
+    
+    function submit(searchString: String) {
+        console.log(searchString)
+
+        fetch(`https://api.github.com/search/repositories?q=${searchString}`)
+            .then(response => response.json())
+            .then(response => response as GitHubSearchResponse)
+            .then(response => setSearchResults(response.items))
+            .catch(console.log)
+    }
+
     return <div>
+        <input
+            type="text"
+            className={styles.search}
+            placeholder="search"
+            onKeyUp={event => {
+                if ( event.key === "Enter" ) {
+                    let inputValue = (event.target as HTMLInputElement).value
+                    submit(inputValue)
+                }
+            }}
+        />
+        {searchResults.map(repo => <div key={repo.id}>{repo.name}</div>)}
     </div>
 }
+
