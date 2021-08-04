5259e80c5d6a6e8f4829ca9f554c8deeffb8495d
FelixMo42 <felix@tacocat.com>
2021-08-04 15:50:42 -0700

    filter by language

--- src/features/search/Search.tsx
+++ src/features/search/Search.tsx
@@ -2,8 +2,13 @@ import React, { useState } from 'react'
 import { store } from '../../app/store'
 import { useAppSelector } from '../../app/hooks'
 import styles from './Search.module.css'
-import { setSearchTerm, setSearchSort } from './SearchSlice'
 import Repo, { GitHubRepo } from './Repo'
+import {
+    setSearchTerm,
+    setSearchSort,
+    addLanguage,
+    removeLanguage
+} from './SearchSlice'
 
 interface GitHubSearchResponse {
     items: GitHubRepo[]
@@ -17,6 +22,7 @@ export default function Search() {
 
     let searchTerm = useAppSelector(state => state.search.searchTerm) 
     let searchSort = useAppSelector(state => state.search.searchSort) 
+    let languages = useAppSelector(state => state.search.languages) 
     
     function search() {
         // We shouldnt search if the search term is blank
@@ -26,8 +32,17 @@ export default function Search() {
 
         // https://docs.github.com/en/rest/reference/search#search-repositories
         let searchRequestUrl = [
+            // The url for the REST api.
             "https://api.github.com/search/repositories?q=",
+            
+            // Set the base search term to match against.
             searchTerm,
+
+            // Sets which languages are allowed.
+            // If the list is empty, it will allow all of them.
+            `+language:${languages.join("+")}`,
+
+            // Set the way we want to sort the results.
             `&sort=${searchSort}`
         ].join("")
 
@@ -70,6 +85,32 @@ export default function Search() {
             <option value="forks">forks</option>
             <option value="updated">updated</option>
         </select>
+
+        {/* allowed languages */}
+        <div>{languages.map(language =>
+            <span
+                key={language}
+                className={styles.language}
+                placeholder="language"
+                onClick={event => {
+                    let value = (event.target as HTMLElement).innerHTML
+                    store.dispatch(removeLanguage(value))
+                }}
+            >{language}</span>
+        )}</div>
+        
+        <input
+            type="text"
+            placeholder="language"
+            onKeyUp={event => {
+                if ( event.key === "Enter" ) {
+                    let element = event.target as HTMLInputElement
+                    let value = element.value
+                    element.value = ""
+                    store.dispatch(addLanguage(value))
+                }
+            }
+        }/>
 
         {/* The list of search results */}
         <div>{
--- src/features/search/SearchSlice.tsx
+++ src/features/search/SearchSlice.tsx
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 export interface SearchState {
     searchTerm: string
     searchSort: string
+    languages: string[]
 }
 
 const initialState: SearchState = {
     searchTerm: "",
-    searchSort: "best-match"
+    searchSort: "best-match",
+    languages: []
 }
 
 const counterSlice = createSlice({
@@ -19,10 +21,29 @@ const counterSlice = createSlice({
         },
         setSearchSort(state, action: PayloadAction<string>) {
             state.searchSort = action.payload
+        },
+        addLanguage(state, action: PayloadAction<string>) {
+            // Check too make sure were not adding a duplicate language.
+            for (let language in state.languages) {
+                if ( language === action.payload ) {
+                    return
+                }
+            }
+
+            // Add the language too the list
+            state.languages = [...state.languages, action.payload]
+        },
+        removeLanguage(state, action: PayloadAction<string>) {
+            state.languages = state.languages.filter(language => language !== action.payload)
         }
     }
 })
 
-export const { setSearchTerm, setSearchSort } = counterSlice.actions
+export const {
+    setSearchTerm,
+    setSearchSort,
+    addLanguage,
+    removeLanguage
+} = counterSlice.actions
 
 export default counterSlice.reducer
