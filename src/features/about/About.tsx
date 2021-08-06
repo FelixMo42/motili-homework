2ade71bdf2507a898923a7510bf83d0f823f753a
FelixMo42 <felix@tacocat.com>
2021-08-06 09:41:01 -0700

    added languages to about page

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -12,6 +12,7 @@ interface AboutParams {
 
 export default function About({match: {params: {owner, name}}}: AboutParams) {
     let [repo, setRepo] = useState<GitHubRepo | undefined>()
+    let [languages, setLanguages] = useState<string[]>([])
 
     // The message is whats shown beffor the repo is loaded.
     // Defaults to "loading", but will also display error if loading fails.
@@ -27,6 +28,18 @@ export default function About({match: {params: {owner, name}}}: AboutParams) {
             .catch(err => setMessage(err))
     }, [])
 
+    // Ask the github api for the repo languages.
+    // The keys represent the languages in the repo.
+    // Note: Uses no dependencies so that it dosent resend when the repo is set. 
+    useEffect(() => {
+        fetch(`https://api.github.com/repos/${owner}/${name}/languages`)
+            .then(response => response.json())
+            .then(response => response as Object)
+            .then(response => Object.keys(response))
+            .then(response => setLanguages([...response]))
+            .catch(console.log)
+    }, [])
+
     // If the repo is undefined
     if ( repo === undefined ) {
         return <div>{message}</div>
@@ -37,6 +50,7 @@ export default function About({match: {params: {owner, name}}}: AboutParams) {
         <div>{repo.description}</div>
         <br/>
         <div>stars: {repo.stargazers_count}</div> 
+        <div>languages: {languages.join(", ")}</div> 
     </div>
 }
 
